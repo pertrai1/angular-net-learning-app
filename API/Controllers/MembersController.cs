c8ac1db7378053f6371e177922e495f58f50dced
Rob Simpson <rsimpson2@me.com>
2025-07-30 10:35:52 -0400

    Cloudinary setup and round 1 of image upload

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -10,7 +10,7 @@ namespace API.Controllers
 {
 
     [Authorize]
-    public class MembersController(IMemberRepository memberRepository) : BaseAPIController
+    public class MembersController(IMemberRepository memberRepository, IPhotoService photoService) : BaseAPIController
     {
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
@@ -56,5 +56,35 @@ namespace API.Controllers
             return BadRequest("Failed to update member");
         }
 
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<Photo>> AddPhoto([FromForm] IFormFile file)
+        {
+            var member = await memberRepository.GetMemberForUpdate(User.GetMemberId());
+
+            if (member == null) return BadRequest("Cannot update member");
+
+            var result = await photoService.UploadPhotoAsync(file);
+
+            if (result.Error != null) return BadRequest(result.Error.Message);
+
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId,
+                MemberId = User.GetMemberId()
+            };
+
+            if (member.ImageUrl == null)
+            {
+                member.ImageUrl = photo.Url;
+                member.User.ImageUrl = photo.Url;
+            }
+
+            member.Photos.Add(photo);
+
+            if (await memberRepository.SaveAllAsync()) return photo;
+
+            return BadRequest("Problem adding photo");
+        }
     }
 }
