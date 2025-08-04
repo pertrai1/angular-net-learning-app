98c333e7fb2628e1c43153e80c0613a891f54946
Rob Simpson <rsimpson2@me.com>
2025-08-04 16:39:48 -0400

    Start of pagination in API

--- API/Data/MemberRepository.cs
+++ API/Data/MemberRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,9 +18,10 @@ public class MemberRepository(AppDbContext context) : IMemberRepository
     return await context.Members.Include(x => x.User).Include(x => x.Photos).SingleOrDefaultAsync(x => x.Id == id);
   }
 
-  public async Task<IReadOnlyList<Member>> GetMembersAsync()
+  public async Task<PaginatedResult<Member>> GetMembersAsync(PagingParams pagingParams)
   {
-    return await context.Members.ToListAsync();
+    var query = context.Members.AsQueryable();
+    return await PaginationHelper.CreateAsync(query, pagingParams.PageNumber, pagingParams.PageSize);
   }
 
   public async Task<IReadOnlyList<Photo>> GetPhotosForMemberAsync(string memberId)
