e24e46a431bc3035ad1b7e9fc781fa5fe52b86da
Rob Simpson <rsimpson2@me.com>
2025-08-11 15:35:38 -0400

    Finish filters, sorting, pagination

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -18,7 +18,15 @@ export class MemberList implements OnInit {
   protected paginatedMembers = signal<PaginatedResult<Member> | null>(null);
 
   protected memberParams = new MemberParams();
+  private updatedParams = new MemberParams();
 
+  constructor() {
+    const filters = localStorage.getItem('filters');
+    if (filters) {
+      this.memberParams = JSON.parse(filters);
+      this.updatedParams = JSON.parse(filters);
+    }
+  }
   ngOnInit() {
     this.loadMembers();
   }
@@ -46,12 +54,42 @@ export class MemberList implements OnInit {
   }
 
   onFilterChange(data: MemberParams) {
-    this.memberParams = data;
+    this.memberParams = { ...data };
+    this.updatedParams = { ...data };
     this.loadMembers();
   }
 
   resetFilters() {
     this.memberParams = new MemberParams();
+    this.updatedParams = new MemberParams();
     this.loadMembers();
+  }
+
+  get displayMessage(): string {
+    const defaultParams = new MemberParams();
+    const filters: string[] = [];
+    if (this.updatedParams.gender) {
+      filters.push(this.updatedParams.gender + 's');
+    } else {
+      filters.push('Males', 'Females');
+    }
+    if (
+      this.updatedParams.minAge !== defaultParams.minAge ||
+      this.updatedParams.maxAge !== defaultParams.maxAge
+    ) {
+      filters.push(
+        ` ages ${this.updatedParams.minAge}-${this.updatedParams.maxAge}`
+      );
+    }
+
+    filters.push(
+      this.memberParams.orderBy === 'lastActive'
+        ? 'Recently active'
+        : 'Newest members'
+    );
+
+    return filters.length > 0
+      ? `Selected: ${filters.join(' | ')}`
+      : 'All members';
   }
 }
