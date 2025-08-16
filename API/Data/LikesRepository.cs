7bab76f73b839445dc75b756c308563680e20f3c
Rob Simpson <rsimpson2@me.com>
2025-08-16 15:18:05 -0400

    Start likes

---
+++ API/Data/LikesRepository.cs
@@ -0,0 +1,50 @@
+using System;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class LikesRepository(AppDbContext context) : ILikesRepository
+{
+  public void AddLike(MemberLike like)
+  {
+    context.Likes.Add(like);
+  }
+
+  public void DeleteLike(MemberLike like)
+  {
+    context.Likes.Remove(like);
+  }
+
+  public async Task<IReadOnlyList<string>> GetCurrentMemberLikeIds(string memberId)
+  {
+    return await context.Likes.Where(x => x.SourceMemberId == memberId).Select(x => x.TargetMemberId).ToListAsync();
+  }
+
+  public async Task<MemberLike?> GetMemberLike(string sourceMemberId, string targetMemberId)
+  {
+    return await context.Likes.FindAsync(sourceMemberId, targetMemberId);
+  }
+
+  public async Task<IReadOnlyList<Member>> GetMemberLikes(string predicate, string memberId)
+  {
+    var query = context.Likes.AsQueryable();
+
+    switch (predicate)
+    {
+      case "liked":
+        return await query.Where(x => x.SourceMemberId == memberId).Select(x => x.TargetMember).ToListAsync();
+      case "likedBy":
+        return await query.Where(x => x.TargetMemberId == memberId).Select(x => x.SourceMember).ToListAsync();
+      default: // mutual
+        var likeIds = await GetCurrentMemberLikeIds(memberId);
+        return await query.Where(x => x.TargetMemberId == memberId && likeIds.Contains(x.SourceMemberId)).Select(x => x.SourceMember).ToListAsync();
+    }
+  }
+
+  public async Task<bool> SaveAllChanges()
+  {
+    return await context.SaveChangesAsync() > 0;
+  }
+}
--- client/src/features/members/member-card/member-card.ts
+++ client/src/features/members/member-card/member-card.ts
@@ -1,7 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { Member } from '../../../types/member';
 import { RouterLink } from '@angular/router';
 import { AgePipe } from '../../../core/pipes/age-pipe';
+import { LikesService } from '../../../core/services/likes-service';
 
 @Component({
   selector: 'app-member-card',
@@ -10,5 +11,28 @@ import { AgePipe } from '../../../core/pipes/age-pipe';
   styleUrl: './member-card.css',
 })
 export class MemberCard {
+  private likesServices = inject(LikesService);
   member = input.required<Member>();
+
+  protected hasLiked = computed(() => {
+    return this.likesServices.likeIds().includes(this.member().id);
+  });
+
+  toggleLike(event: Event) {
+    event.stopPropagation();
+    this.likesServices.toggleLike(this.member().id).subscribe({
+      next: () => {
+        if (this.hasLiked()) {
+          this.likesServices.likeIds.update((ids) =>
+            ids.filter((id) => id !== this.member().id)
+          );
+        } else {
+          this.likesServices.likeIds.update((ids) => [
+            ...ids,
+            this.member().id,
+          ]);
+        }
+      },
+    });
+  }
 }
